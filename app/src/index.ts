53ed5ceb9e126ba37c7d6e4555a9be3f0f09e1e3
Icaro90 <icaro.nove-nta@hotmail.com>
2024-03-13 19:04:24 -0300

    feature: config inicial lambda

---
+++ app/src/index.ts
@@ -0,0 +1,28 @@
+import {
+  AuthFlowType,
+  paginateListUserPools,
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
+
+export const handler = async (event: any) => {
+  const login = event.headers.login;
+  const password = event.headers.password;
+
+  const client = new CognitoIdentityProviderClient({});
+
+  const command = new InitiateAuthCommand({
+    AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+    AuthParameters: {
+      USERNAME: login,
+      PASSWORD: password
+    },
+    ClientId: 'hk6lfc2knq0q6mnpdoov9hvrk'
+  });
+
+  const response = await client.send(command)
+
+  console.log(response)
+
+  return response
+}
